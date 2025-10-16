582cfcefbb3c19da0a6d7291db6f85df58e8bea8
jacopo-derrico <derricoj.design@gmail.com>
2025-10-16 16:53:23 +0200

    install tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,5 +26,6 @@ export default defineConfig({
         ? undefined
         : {},
     }),
+    tailwindcss(),
   ],
 })
